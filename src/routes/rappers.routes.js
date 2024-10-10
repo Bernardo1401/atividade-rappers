3b6f5edfc8f93941435af23e888f8a9e344f4183
Bernardo Marques <sardinhaber@gmail.com>
2024-10-10 09:46:07 -0300

    feat: add comments in code

--- src/routes/rappers.routes.js
+++ src/routes/rappers.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express"
 
 const rappersRoutes = Router()
-
+//Array de rappers pré-cadastrados
 let rappers = [
 {
     id: Number(Math.floor(Math.random() * 99) + 1),
@@ -27,19 +27,19 @@ rappersRoutes.get("/", (req, res) => {
      // Rota para cadastrar um novo rapper
 rappersRoutes.post("/", (req, res) => {
     const { nome, idade, descricao, suspeito} = req.body
-
-    if(!nome) {
+//Validação dos campos nome e idade para efetuação do cadastro
+    if(!nome || !idade) {
         return res.status(400).send({
-            message: "O campo nome é obrigatório para o cadastro"
+            message: "Os campos nome e idade são obrigatórios para o cadastro" 
         })
     }
-    
+    //Validação se o rapper é suspeito ou não
     if(suspeito != "sim" && suspeito != "não"){
         return res.status(400).send({
-            message: "Digite sim ou não"
+            message: "Digite sim ou não" 
     })
     }
-
+//Validação para que tenha apenas números inteiros
     if(Number.isInteger(idade) == false){
         return res.status(400).send({
             message: "A idade do suspeito deve ser em números inteiros"
@@ -68,7 +68,7 @@ rappersRoutes.get("/:id", (req, res) => {
     const rapper = rappers.find((song) => 
         song.id === Number(id)
     )
-
+ //Verificação se o rapper foi encontrado
     if (!rapper) {
         return res.status(404).send({message: "Rapper não encontrado!" })
     }
@@ -80,11 +80,11 @@ rappersRoutes.get("/:id", (req, res) => {
 rappersRoutes.put("/:id", (req, res) => {
     const { id } = req.params
 
-    
+     // Busca um rapper pelo id no array de rappers
     const rapper = rappers.find((song) => 
         song.id === Number(id)
     )
-
+ //Verificação se o rapper foi encontrado
     if (!rapper) {
         return res.status(404).send({message: "Rapper não não encontrado!" })
     }
@@ -103,18 +103,18 @@ rappersRoutes.put("/:id", (req, res) => {
     })
 })
 
-// Rota para deleter uma guloseima
+// Rota para deleter um rapper
 rappersRoutes.delete("/:id", (req, res) => {
     const { id } = req.params
 
     let rapper = rappers.find((song) => 
         song.id === Number(id)
     )
-    
+    //Verificação se o rapper foi encontrado
         if (!rapper) {
         return res.status(404).send({message:"Rapper não encontrado!" })
     } 
-
+    //Remove o rapper do array rappers
     rappers =  rappers.filter((song) => song.id !== Number(id) )
 
     return res.status(200).send({
